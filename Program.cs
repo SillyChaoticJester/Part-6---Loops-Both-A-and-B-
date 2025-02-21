2cb11609ba01161fd2517d396a7ab705916c23ea
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-21 13:50:53 -0500

    Doing the tutorial

--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@
         {
             //Part 6a - Counted Loops
 
+            for (int i = 0; i < 10; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Counted For Statements have three different elements
+
+            //1. Initialization (i = 0): Starts the counter, goes through once before loop begins
+            //2. Termination (i <= 10): Loop continues if True, stops when False.
+            //3. Increment (i++): Adds up/Counts down from the counter.
+
+            //Changing <= to < will make it so it doesn't add the extra loop. 
+
 
         }
     }
