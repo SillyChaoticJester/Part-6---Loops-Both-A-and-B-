7e88baaa66df4ad072de426b82865842b6ab6f15
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-21 13:31:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Part_6___Loops__Both_A_and_B_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 6a - Counted Loops
+
+
+        }
+    }
+}
